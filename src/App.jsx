67497d69d0362feb9cea5c4f0fe21bab4d46ffbf
Almos Mali <almos0002@gmail.com>
2024-12-27 13:14:02 +0545

    This is Gandu Change

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,9 @@
-export const App = () => {
-  return <h1>This is Almos Mali</h1>;
-};
+// export const App = () => {
+//   return <h1>This is React with Vite | Almos </h1>;
+// };
+
+function App() {
+  return <h1>This is React With Vite | Almos Malakar</h1>;
+}
+
+export default App;
