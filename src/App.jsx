be84986a0b8da323ab7665967da869d728534882
Almos Mali <almos0002@gmail.com>
2024-12-29 13:14:50 +0545

    This is Ultimate Gandu Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
-// export const App = () => {
-//   return <h1>This is React with Vite | Almos </h1>;
-// };
-
-import { Fragment } from "react";
+import { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const addValue = () => {
+    setCounter(counter + 1);
+  };
+
+  const remValue = () => {
+    if (counter > 0) {
+      setCounter(counter - 1);
+    } else {
+      console.log("Yetroo ni Jadina 0 vanda Muni", Math.random());
+    }
+  };
   return (
     <>
       <h1>This is React With Vite | Almos Malakar</h1>
       <button>Click me</button>
+
+      {/* React Hooks */}
+      <h2>Counter Value {counter}</h2>
+      <button onClick={addValue}>Add Value {counter}</button>
+      <button onClick={remValue}>Remo Value {counter}</button>
+
+      {/* Tailwind Css and Props */}
     </>
   );
 }
